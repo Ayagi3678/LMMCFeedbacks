9098908ac26c92ac26cb16b33f079281d8cee740
Ayagi3678 <114006559+Ayagi3678@users.noreply.github.com>
2023-12-30 21:34:34 +0900

    CanvasGroupAlphaFeedback

--- Assets/LMMCFeedbacks/Runtime/Feedbacks/CanvasGroupAlphaFeedback.cs
+++ Assets/LMMCFeedbacks/Runtime/Feedbacks/CanvasGroupAlphaFeedback.cs
@@ -37,7 +37,7 @@ namespace LMMCFeedbacks
         public MotionHandle Create()
         {
             Cancel();
-            initialAlpha = target.alpha;
+            InitialSetup();
             var builder = LMotion.Create(zero, one, durationTime).WithDelay(options.delayTime)
                 .WithIgnoreTimeScale(options.ignoreTimeScale)
                 .WithLoops(options.loop ? options.loopCount : 1, options.loopType)
