04562380bf3fedf62234fac485989d6a0ae7553e
Ayagi3678 <114006559+Ayagi3678@users.noreply.github.com>
2024-01-05 03:14:20 +0900

    バグ修正

--- Assets/LMMCFeedbacks/Runtime/Singletons/FeedbackSoundManager.cs
+++ Assets/LMMCFeedbacks/Runtime/Singletons/FeedbackSoundManager.cs
@@ -6,28 +6,27 @@ namespace LMMCFeedbacks.Runtime.Managers
     public class FeedbackSoundManager : MonoBehaviour
     {
         private static FeedbackSoundManager _instance;
-        private AudioSource _audioSource;
 
-        public AudioSource AudioSource
-        {
-            get
-            {
-                if (_audioSource != null) return _audioSource;
-                _audioSource = GetComponent<AudioSource>();
-                return _audioSource;
-            }
-            private set => _audioSource = value;
-        }
+        public AudioSource AudioSource { get; private set; }
 
         public static FeedbackSoundManager Instance
         {
             get
             {
-                if (_instance != null) return _instance;
+                if (_instance != null)
+                {
+                    _instance.Setup();
+                    return _instance;
+                }
+
                 var type = typeof(FeedbackSoundManager);
 
                 _instance = (FeedbackSoundManager)FindObjectOfType(type);
-                if (_instance != null) return _instance;
+                if (_instance != null)
+                {
+                    _instance.Setup();
+                    return _instance;
+                }
 
                 var gameObject = new GameObject("FeedbackSoundManager", type);
                 _instance = gameObject.GetComponent<FeedbackSoundManager>();
@@ -35,6 +34,7 @@ namespace LMMCFeedbacks.Runtime.Managers
                 if (_instance == null)
                     Debug.LogError("Problem during the creation of FeedbackSoundManager", gameObject);
 
+                _instance.Setup();
                 return _instance;
             }
         }
@@ -54,7 +54,12 @@ namespace LMMCFeedbacks.Runtime.Managers
             AudioSource.Stop();
         }
 
-        private bool Initialize()
+        public void Setup()
+        {
+            if (AudioSource == null) AudioSource = GetComponent<AudioSource>();
+        }
+
+        public bool Initialize()
         {
             if (_instance == null)
             {
